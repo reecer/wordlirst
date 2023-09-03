cbc6d9ff721cd0562d8a94add6fe3ec37e406b69
Reece Robinson <reecekrobinson@gmail.com>
2023-09-02 21:54:52 -0800

    use itertools for permutations

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod generate;
 
 use clap::Parser;
 use generate::{generate_wordlist, ReplacePair};
+use itertools::Itertools;
 use regex_syntax::{hir, hir::Hir, hir::HirKind, parse};
 use std::error::Error;
 use std::fmt;
@@ -98,12 +99,7 @@ fn main() -> io::Result<()> {
     };
     println!("{}", gen_info);
 
-    let mut regex = args.pattern.expect("Testing regex");
-
-    // Ensure $ to signify end of string
-    if regex.chars().last() != Some('$') {
-        regex.push('$');
-    }
+    let regex = args.pattern.expect("Testing regex");
 
     // Create AST
     let ast = parse(&regex).expect("regex is invalid");
@@ -111,35 +107,36 @@ fn main() -> io::Result<()> {
 
     // Walk AST recursively
     println!("{:#?}", ast);
-    let mut string = String::new();
-    walk_regex(&ast, &mut string, &gen_info, 0);
+    let result = walk_regex(&ast, &gen_info);
+
+    println!("RESULT:");
+    for word in &result {
+        println!("{}", word);
+    }
+
+    println!("{} words generated!", result.len());
 
     // generate 'em
     // println!("Generating...");
     // generate_wordlist(&fout, dictionary, terms, transforms, min_length, max_length)?;
 
     // count resulting lines
-    fout.seek(std::io::SeekFrom::Start(0))?;
-    let line_count = BufReader::new(&fout).lines().count();
-    println!("{} words generated!", line_count);
+    // fout.seek(std::io::SeekFrom::Start(0))?;
+    // let line_count = BufReader::new(&fout).lines().count();
+    // println!("{} words generated!", line_count);
 
     Ok(())
 }
 
 // "(%t){3}(199[0-9]|20(0[0-9]|1[0-9]|2[0-3]))"
 // 2 terms + 1990-99 | 2000-2023
-fn walk_regex(ast: &Hir, candidate: &mut String, gen: &Generator, depth: usize) {
-    if candidate.len() >= gen.max_length {
-        return;
-    }
+fn walk_regex(ast: &Hir, gen: &Generator) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
 
-    for _ in 0..depth {
-        print!("- ");
-    }
     match ast.kind() {
         HirKind::Look(c) => match c {
             hir::Look::End => {
-                println!("END OF LINE. TODO: add '{}'", candidate);
+                println!("END OF LINE.");
             }
             _ => {
                 println!("LOOK: {:#?}", c);
@@ -150,80 +147,108 @@ fn walk_regex(ast: &Hir, candidate: &mut String, gen: &Generator, depth: usize) 
         }
         HirKind::Literal(c) => match &*c.0 {
             b"%t" => {
-                println!("FOUND TERM");
+                for t in gen.terms {
+                    result.push(t.clone());
+                }
             }
             b"%w" => {
-                println!("FOUND WORD");
-            }
-            _ => {
-                println!("LITERAL: {:#?}", c);
-            }
-        },
-
-        // end recursion here?
-        HirKind::Class(c) => match c {
-            hir::Class::Unicode(s) => {
-                if s.is_ascii() {
-                    let all_chars = s.ranges().iter().map(|x| x.start()..=x.end()).flatten();
-                    println!("EXTENDING WORD: '{}'", candidate);
-                    for ch in all_chars {
-                        let mut word = candidate.clone();
-                        word.push(ch);
-                        println!("ADD WORD: '{}'", word);
-                        walk_regex(ast, &mut word, gen, depth);
-                    }
-                } else {
-                    let all_len: usize = s.ranges().iter().map(|x| x.len()).sum();
-                    print!("Non-ascii values ({})", all_len);
+                for w in gen.dictionary {
+                    result.push(w.clone());
                 }
-                println!("");
-                // println!("CLASS: {:#?}", s.ranges());
             }
             _ => {
-                println!("CLASS IS BYTES???");
+                result.push(std::str::from_utf8(&c.0).unwrap().to_string());
             }
         },
-        HirKind::Repetition(c) => {
-            // TODO: max default should be max_length?
-            println!(
-                "REPETITION({}, {}):",
-                c.min,
-                c.max.unwrap_or(std::f32::INFINITY as u32)
-            );
-            // walk_regex(&c.sub, candidate, gen, depth + 1);
 
-            // while candidate.len() < max_length
-            // -> add to candidate
-            for _ in 0..c.min {
-                walk_regex(&c.sub, candidate, gen, depth + 1);
+        // this contains literal values
+        HirKind::Class(hir::Class::Unicode(s)) => {
+            if s.is_ascii() {
+                let all_chars = s.iter().map(|x| x.start()..=x.end()).flatten();
+                // let char_count: usize = s.iter().map(|x| x.len()).sum();
+                // println!("Added {} chars", char_count);
+                for ch in all_chars {
+                    // let mut word = candidate.clone();
+                    // word.push(ch);
+                    result.push(ch.to_string());
+                }
+            } else {
+                let all_len: usize = s.ranges().iter().map(|x| x.len()).sum();
+                print!("Non-ascii values ({})", all_len);
+            }
+        }
+        HirKind::Class(hir::Class::Bytes(s)) => {
+            println!("CLASS IS BYTES??? {:#?}", s);
+        }
+        HirKind::Repetition(c) => {
+            let max = c.max.unwrap_or(gen.max_length as u32);
+
+            if c.min == 0 {
+                result.push(String::new());
             }
 
-            // continue walk
-
-            // for i in min..max
-            // -> break if candidate.len() > max_length
-            // -> continue walk
+            let words = walk_regex(&c.sub, gen);
+            // println!("Permutating {} words {} times", words.len(), (max - c.min));
+            for i in c.min..=max {
+                for word in (1..=i).map(|_| &words).multi_cartesian_product() {
+                    result.push(word.into_iter().join(""));
+                }
+            }
         }
         HirKind::Capture(c) => {
-            // capture's are just separators for this usage
-            // println!("CAPTURE: {:#?}", c);
-            print!("\r");
-            walk_regex(&c.sub, candidate, gen, depth);
-        }
-        HirKind::Concat(coll) => {
-            // println!("CONCAT: {:#?}", coll);
-            println!("CONCAT:");
-            for x in coll {
-                walk_regex(x, candidate, gen, depth + 1);
+            // capture's are just groups...we don't really care
+            for word in walk_regex(&c.sub, gen) {
+                result.push(word);
             }
         }
-        HirKind::Alternation(coll) => {
-            println!("ALTERNATION:");
+        HirKind::Concat(coll) => {
+            let mut groups: Vec<Vec<String>> = Vec::new();
+            let mut count = 0;
             for x in coll {
-                walk_regex(x, candidate, gen, depth + 1);
+                let group = walk_regex(x, gen);
+                count += group.len();
+                groups.push(group.clone());
+            }
+
+            println!(
+                "Concating {} groups with {} total words",
+                groups.len(),
+                count
+            );
+            let all_perms: std::collections::HashSet<String> = groups
+                .iter()
+                .map(|sublist| sublist.iter())
+                .multi_cartesian_product()
+                .filter(|combination| {
+                    let total_length = combination.iter().map(|s| s.len()).sum::<usize>();
+                    total_length >= gen.min_length && total_length <= gen.max_length
+                })
+                .map(|combo| combo.into_iter().join(""))
+                .collect();
+
+            // println!("{} total permutations", all_perms.into_iter().count());
+            println!("Done concating {}", all_perms.len());
+
+            // let unique_words: std::collections::HashSet<String> = all_perms.into_iter().collect();
+            // for word in unique_words {
+            for word in all_perms {
+                // if !result.contains(&word) {
+                result.push(word);
+                // }
+            }
+
+            println!("Done again");
+        }
+        HirKind::Alternation(coll) => {
+            for x in coll {
+                for word in walk_regex(x, gen) {
+                    result.push(word);
+                }
             }
         } // _ => println!("NO MATCH: {}", ast),
     }
+
+    result
 }
 
 fn read_dictionary(filename: PathBuf, max_length: usize) -> io::Result<Vec<String>> {
