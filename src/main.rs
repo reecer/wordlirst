c93ec60628f5a71d9bedac6f8b4fd78fbac2b028
Reece Robinson <reecekrobinson@gmail.com>
2023-09-20 01:18:25 +0000

    parallel repeat. simpler concat. a few tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 mod generate;
 
 use clap::Parser;
+use clap::builder::PossibleValue;
 use generate::{generate_wordlist, ReplacePair};
-use itertools::Itertools;
+use itertools::{Itertools, iproduct};
 use regex_syntax::{hir, hir::Hir, hir::HirKind, parse};
 use std::error::Error;
 use std::fmt;
 use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, BufReader, Seek};
 use std::path::PathBuf;
+use rayon::prelude::*;
 
 /// Simple program to generate a wordlist
 #[derive(Parser, Debug)]
@@ -39,15 +41,15 @@ struct Args {
     pattern: Option<String>,
 }
 
-struct Generator<'a> {
+struct Generator {
     min_length: usize,
     max_length: usize,
-    transforms: &'a [ReplacePair],
-    dictionary: &'a [String],
-    terms: &'a [String],
+    transforms: Vec<ReplacePair>,
+    dictionary: Vec<String>,
+    terms: Vec<String>,
 }
 
-impl<'a> fmt::Display for Generator<'a> {
+impl<'a> fmt::Display for Generator {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Generator {{\n")?;
         // Print info
@@ -93,21 +95,14 @@ fn main() -> io::Result<()> {
     let gen_info = Generator {
         min_length,
         max_length,
-        transforms: &transforms,
-        terms: &terms,
-        dictionary: &dictionary,
+        transforms: transforms,
+        terms: terms,
+        dictionary: dictionary,
     };
     println!("{}", gen_info);
 
     let regex = args.pattern.expect("Testing regex");
-
-    // Create AST
-    let ast = parse(&regex).expect("regex is invalid");
-    println!("Regex: {}", regex);
-
-    // Walk AST recursively
-    println!("{:#?}", ast);
-    let result = walk_regex(&ast, &gen_info);
+    let result = regex_words(&regex, &gen_info);
 
     println!("RESULT:");
     for word in &result {
@@ -128,6 +123,27 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn regex_words(input: &str, gen: &Generator) -> Vec<String> {
+    // Create AST
+    let ast = parse(input).expect("regex is invalid");
+    println!("Regex: {}", input);
+
+    // Walk AST recursively
+    println!("{:#?}", ast);
+    let possible = walk_regex(&ast, &gen);
+
+    println!("Before filter: {}", possible.len());
+    let result: Vec<String> = possible.into_iter()
+    .filter(|word| {
+        // max_length is already checked at concat and repeat...where else could it happen?
+        return word.len() >= gen.min_length;
+    })
+    .collect();
+    println!("After filter: {}", result.len());
+
+    return result;
+}
+
 // "(%t){3}(199[0-9]|20(0[0-9]|1[0-9]|2[0-3]))"
 // 2 terms + 1990-99 | 2000-2023
 fn walk_regex(ast: &Hir, gen: &Generator) -> Vec<String> {
@@ -141,27 +157,25 @@ fn walk_regex(ast: &Hir, gen: &Generator) -> Vec<String> {
             _ => {
                 println!("LOOK: {:#?}", c);
             }
-        },
+        }
         HirKind::Empty => {
             println!("EMPTY: {}", ast);
         }
         HirKind::Literal(c) => match &*c.0 {
             b"%t" => {
-                for t in gen.terms {
+                for t in &gen.terms {
                     result.push(t.clone());
                 }
             }
             b"%w" => {
-                for w in gen.dictionary {
+                for w in &gen.dictionary {
                     result.push(w.clone());
                 }
             }
             _ => {
                 result.push(std::str::from_utf8(&c.0).unwrap().to_string());
             }
-        },
-
-        // this contains literal values
+        }
         HirKind::Class(hir::Class::Unicode(s)) => {
             if s.is_ascii() {
                 let all_chars = s.iter().map(|x| x.start()..=x.end()).flatten();
@@ -194,15 +208,21 @@ fn walk_regex(ast: &Hir, gen: &Generator) -> Vec<String> {
                 0
             } as u32;
             let last = (max + shortest - 1) / shortest;
-            for i in c.min..=last {
-                println!("Repeat {}", i);
-                for word in (1..=i).map(|_| &words).multi_cartesian_product() {
+            let range = c.min..=last;
+            let words: Vec<Vec<String>> = range.into_par_iter().map(|i| {
+                // println!("Repeat {}", i);
+                let x = (1..=i).map(|_| &words).multi_cartesian_product().filter_map(|word| {
                     let text = word.into_iter().join("");
-                    if text.len() >= gen.min_length && text.len() <= gen.max_length {
-                        result.push(text);
+                    // only check max isn't exceeded
+                    if text.len() <= gen.max_length {
+                        return Some(text);
                     }
-                }
-            }
+                    None
+                }).collect();
+                println!("Repeat {} done. Max = {}", i, gen.max_length);
+                x
+            }).collect();
+            result.extend(words.iter().cloned().flatten());
         }
         HirKind::Capture(c) => {
             // capture's are just groups...we don't really care
@@ -224,25 +244,17 @@ fn walk_regex(ast: &Hir, gen: &Generator) -> Vec<String> {
                 groups.len(),
                 count
             );
-            // let all_perms: std::collections::HashSet<String> = groups
-            let all_perms = groups
-                .iter()
-                .map(|sublist| sublist.iter())
-                .multi_cartesian_product()
-                .filter(|combination| {
-                    let total_length = combination.iter().map(|s| s.len()).sum::<usize>();
-                    total_length >= gen.min_length && total_length <= gen.max_length
-                })
-                .map(|combo| combo.into_iter().join(""));
-            // .collect();
 
-            // println!("{} total permutations", all_perms.into_iter().count());
-            // println!("Done concating {}", all_perms.len());
+            let cartesian = groups.iter().multi_cartesian_product();
+            println!("Cartesian concat done");
 
-            // let unique_words: std::collections::HashSet<String> = all_perms.into_iter().collect();
-            result.extend(all_perms);
+            for x in cartesian {
+                if x.iter().map(|y| y.len()).sum::<usize>() <= gen.max_length {
+                    result.push(x.iter().join(""));
+                }
+            }
 
-            println!("Done again");
+            println!("Done joining concat results");
         }
         HirKind::Alternation(coll) => {
             for x in coll {
@@ -275,4 +287,102 @@ fn parse_key_val(s: &str) -> Result<ReplacePair, Box<dyn Error + Send + Sync + '
         .find('=')
         .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{s}`"))?;
     Ok(ReplacePair(s[..pos].parse()?, s[pos + 1..].parse()?))
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn empty_gen(min: usize, max: usize) -> Generator {
+        Generator {
+            min_length: min,
+            max_length: max,
+            transforms: vec![],
+            terms: vec![],
+            dictionary: vec![],
+        }
+    }
+
+    #[test]
+    fn play() {
+
+    }
+
+    #[test]
+    fn simple_concat() {
+        let gen = empty_gen(2, 2);
+        let pattern = "[a-c][1-3]";
+        let expected = vec!["a1", "b2", "c3"];
+
+        expect(&gen, pattern, expected, 9);
+    }
+    #[test]
+    fn repeat_1() {
+        let gen1 = empty_gen(1, 1);
+        let pattern1 = "[a-z]+";
+        let expected1 = vec!["a", "m", "z"];
+        expect(&gen1, pattern1, expected1, 26);
+    }
+
+    #[test]
+    fn repeat_2() {
+        let gen = empty_gen(2, 2);
+        let pattern = "[a-c]+";
+        let expected = vec!["aa", "bc", "cc"];
+        expect(&gen, pattern, expected, 9);
+    }
+
+    #[test]
+    fn repeat_1_2() {
+        let gen2 = empty_gen(1, 2);
+        let pattern2 = "[a-z]+";
+        let expected2 = vec!["a", "mt", "zz"];
+        expect(&gen2, pattern2, expected2, 26*26 + 26);
+    }
+
+    #[test]
+    fn concat_repeat_2_5() {
+        let min = 2;
+        let max = 5;
+        let gen = empty_gen(min, max);
+        let pattern = "[a-d]+[1-6]+";
+        let expected = vec!["a3", "abcb1", "b2312", "cca33", "cc2", "abc3"];
+
+        let count = calculate_total_creations(4, 6, min..=max);
+        let _res = expect(&gen, pattern, expected, count);
+        // println!("{}", res.join("\n"));
+        // assert!(false);
+    }
+
+    fn expect(gen: &Generator, pattern: &str, expected: Vec<&str>, count: usize) -> Vec<String> {
+        let result = regex_words(pattern, &gen);
+
+        assert_eq!(result.len(), count, "RESULT: {:?}", &result[0..50]);
+        for s in expected {
+            assert!(result.contains(&s.to_string()), "doesn't contain {}", s);
+        }
+        return result;
+    }
+
+
+    // [a-d]+[1-3]+
+    // 3 => 84 == 4^2 * 3^1 + 4^1 * 3^2
+    // 4 => 444 == 4^3 * 3^1 + 4^2 * 3^2 + 4^1*3^3
+    // 5 => 2100 == 4^4 * 3^1 + 4^3 * 3^2 + 4^2 * 3^3 + 4^1 * 3^4
+    fn calculate_total_creations(pool1_size: usize, pool2_size: usize, length_range: std::ops::RangeInclusive<usize>) -> usize {
+        length_range
+            .map(|string_length| {
+                let mut total = 0;
+                
+                for i in 1..string_length {
+                    let pool1_contrib = pool1_size.pow((string_length - i) as u32);
+                    let pool2_contrib = pool2_size.pow(i as u32);
+                    total += pool1_contrib * pool2_contrib;
+                }
+                
+                total
+            })
+            .sum()
+    }
 }
